0e93281b83384949a06d8cdd0e0cf83bd18be083
Jian <jiandagonay098@gmail.com>
2023-01-22 16:35:04 +0800

    Added logo homepage

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -9,8 +9,8 @@
         <header class="bg-dark py-5">
             <div class="container px-4 px-lg-5 my-5">
                 <div class="text-center text-white">
-                    <h1 class="display-4 fw-bolder">Shop in style</h1>
-                    <p class="lead fw-normal text-white-50 mb-0">With this shop hompeage template</p>
+                    <h1 class="display-4 fw-bolder">“... the pleasures of vaping are distinct from those of smoking.”</h1>
+                    <p class="lead fw-normal text-white-50 mb-0">– Esther Wang</p>
                 </div>
             </div>
         </header>
@@ -21,14 +21,14 @@
                     <div class="col mb-5">
                         <div class="card h-100">
                             <!-- Product image-->
-                            <img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
+                            <img class="card-img-top" src="https://cdn.shopify.com/s/files/1/0168/4652/0384/products/314495394_444322381216819_6499638323517307181_n_1024x1024.jpg?v=1668319348" alt="..." />
                             <!-- Product details-->
                             <div class="card-body p-4">
                                 <div class="text-center">
                                     <!-- Product name-->
-                                    <h5 class="fw-bolder">Fancy Product</h5>
+                                    <h5 class="fw-bolder">Raspberry Lychee</h5>
                                     <!-- Product price-->
-                                    $40.00 - $80.00
+                                    ₱400
                                 </div>
                             </div>
                             <!-- Product actions-->
@@ -42,12 +42,12 @@
                             <!-- Sale badge-->
                             <div class="badge bg-dark text-white position-absolute" style="top: 0.5rem; right: 0.5rem">Sale</div>
                             <!-- Product image-->
-                            <img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
+                            <img class="card-img-top" src="https://ph-test-11.slatic.net/p/78ebb1c24ec97fece9d3cd11d8dda18f.jpg" alt="..." />
                             <!-- Product details-->
                             <div class="card-body p-4">
                                 <div class="text-center">
                                     <!-- Product name-->
-                                    <h5 class="fw-bolder">Special Item</h5>
+                                    <h5 class="fw-bolder">Apple Lime</h5>
                                     <!-- Product reviews-->
                                     <div class="d-flex justify-content-center small text-warning mb-2">
                                         <div class="bi-star-fill"></div>
@@ -57,14 +57,13 @@
                                         <div class="bi-star-fill"></div>
                                     </div>
                                     <!-- Product price-->
-                                    <span class="text-muted text-decoration-line-through">$20.00</span>
-                                    $18.00
+                                    <span class="text-muted text-decoration-line-through">₱600</span>
+                                    ₱400
                                 </div>
                             </div>
                             <!-- Product actions-->
                             <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
                                 <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#">Add to cart</a></div>
-                                <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#">Log in</a></div>
 
                             </div>
                         </div>
@@ -74,15 +73,15 @@
                             <!-- Sale badge-->
                             <div class="badge bg-dark text-white position-absolute" style="top: 0.5rem; right: 0.5rem">Sale</div>
                             <!-- Product image-->
-                            <img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
+                            <img class="card-img-top" src="https://cdn.shopify.com/s/files/1/0168/4652/0384/products/tobacco_480x.jpg?v=1669429315" alt="..." />
                             <!-- Product details-->
                             <div class="card-body p-4">
                                 <div class="text-center">
                                     <!-- Product name-->
-                                    <h5 class="fw-bolder">Sale Item</h5>
+                                    <h5 class="fw-bolder">Mosmo Storm 6000 puffs</h5>
                                     <!-- Product price-->
-                                    <span class="text-muted text-decoration-line-through">$50.00</span>
-                                    $25.00
+                                    <span class="text-muted text-decoration-line-through">₱799</span>
+                                    ₱500
                                 </div>
                             </div>
                             <!-- Product actions-->
